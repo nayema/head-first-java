42248baafcb4faf1b500d54e197bae743c613b67
Nayema Nasir <nayema.nasir89@gmail.com>
2017-08-10 18:04:38 -0400

    Experiments with Wrappers

---
+++ chapter10-exercise2/src/com/nayema/Main.java
@@ -0,0 +1,8 @@
+package com.nayema;
+
+public class Main {
+    public static void main(String[] args) {
+        TestBox t = new TestBox();
+        t.go();
+    }
+}
---
+++ chapter10-exercise2/src/com/nayema/TestBox.java
@@ -0,0 +1,12 @@
+package com.nayema;
+
+public class TestBox {
+    Integer i;
+    int j;
+
+    public void go() {
+        j = i;
+        System.out.println(j);
+        System.out.println(i);
+    }
+}
