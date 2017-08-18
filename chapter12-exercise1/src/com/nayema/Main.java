726075122b9eb8058e818840055f2bbb5a80f9b3
Nayema Nasir <nayema.nasir89@gmail.com>
2017-08-18 19:53:03 -0400

    Adds GUI method for clicking button

--- chapter12-exercise1/src/com/nayema/Main.java
+++ chapter12-exercise1/src/com/nayema/Main.java
@@ -4,6 +4,8 @@ import javax.swing.*;
 
 public class Main {
 
+    JButton button;
+
     public static void main(String[] args) {
         JFrame frame = new JFrame();
         JButton button = new JButton("Click Me");
@@ -15,5 +17,9 @@ public class Main {
         frame.setSize(300, 300);
 
         frame.setVisible(true);
+    }
+
+    public void changeIt() {
+        button.setText("I've been clicked!");
     }
 }
