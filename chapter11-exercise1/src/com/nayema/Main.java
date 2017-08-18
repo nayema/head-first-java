959cd01b6d466440534a2a2af758df0c3a3b3267
Nayema Nasir <nayema.nasir89@gmail.com>
2017-08-18 16:07:47 -0400

    Adds change instrument under play method

--- chapter11-exercise1/src/com/nayema/Main.java
+++ chapter11-exercise1/src/com/nayema/Main.java
@@ -4,26 +4,37 @@ import javax.sound.midi.*;
 
 public class Main {
     public static void main(String[] args) {
-        Main mt = new Main();
-        mt.play();
+        Main mini = new Main();
+        if (args.length < 2) {
+            System.out.println("Don't forget the instrument and note args");
+        } else {
+            int instrument = Integer.parseInt(args[0]);
+            int note = Integer.parseInt(args[1]);
+            mini.play(instrument, note);
+        }
     }
 
-    public void play() {
+    public void play(int instrument, int note) {
         try {
             Sequencer player = MidiSystem.getSequencer();
             player.open();
-
             Sequence seq = new Sequence(Sequence.PPQ, 4);
-
             Track track = seq.createTrack();
 
+            MidiEvent event = null;
+
+            ShortMessage first = new ShortMessage();
+            first.setMessage(192, 1, instrument, 0);
+            MidiEvent changeInstrument = new MidiEvent(first, 1);
+            track.add(changeInstrument);
+
             ShortMessage a = new ShortMessage();
-            a.setMessage(144, 1, 44, 100);
+            a.setMessage(144, 1, note, 100);
             MidiEvent noteOn = new MidiEvent(a, 1);
             track.add(noteOn);
 
             ShortMessage b = new ShortMessage();
-            b.setMessage(128, 1, 44, 100);
+            b.setMessage(128, 1, note, 100);
             MidiEvent noteOff = new MidiEvent(b, 16);
             track.add(noteOff);
 
@@ -32,7 +43,6 @@ public class Main {
             player.start();
 
         } catch (Exception ex) {
-            System.out.println("Bummer!i");
             ex.printStackTrace();
 
         } finally {
