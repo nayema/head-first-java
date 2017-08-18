31fe23a5f7b7abae6c804a2b9340bb62a08401f2
Nayema Nasir <nayema.nasir89@gmail.com>
2017-08-18 18:47:12 -0400

    Makes simple GUI

---
+++ chapter12-exercise1/src/com/nayema/Main.java
@@ -0,0 +1,19 @@
+package com.nayema;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        JButton button = new JButton("Click Me");
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        frame.getContentPane().add(button);
+
+        frame.setSize(300, 300);
+
+        frame.setVisible(true);
+    }
+}
