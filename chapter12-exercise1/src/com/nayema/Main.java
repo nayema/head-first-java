29f29f48fba536ef70b4fa043f08f450c264439f
Nayema Nasir <nayema.nasir89@gmail.com>
2017-09-06 15:44:07 -0400

    Implements ActionListener interface

--- chapter12-exercise1/src/com/nayema/Main.java
+++ chapter12-exercise1/src/com/nayema/Main.java
@@ -1,25 +1,31 @@
 package com.nayema;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Main {
-
+public class Main implements ActionListener {
     JButton button;
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame();
-        JButton button = new JButton("Click Me");
+        Main gui = new Main();
+        gui.go();
+    }
 
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    public void go() {
+        JFrame frame = new JFrame();
+        button = new JButton("Click Me");
+
+        button.addActionListener(this);
 
         frame.getContentPane().add(button);
-
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(300, 300);
-
         frame.setVisible(true);
     }
 
-    public void changeIt() {
+    @Override
+    public void actionPerformed(ActionEvent e) {
         button.setText("I've been clicked!");
     }
 }
