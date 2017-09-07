9ce944160bffb07211fa004ce08b244c77cea00c
Nayema Nasir <nayema.nasir89@gmail.com>
2017-09-07 17:53:48 -0400

    Adds a button on each grid of the Border Layout Panel

--- chapter13-exercise1/src/com/nayema/Main.java
+++ chapter13-exercise1/src/com/nayema/Main.java
@@ -4,11 +4,26 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Main {
-
     public static void main(String[] args) {
+        Main gui = new Main();
+        gui.go();
+    }
+
+    public void go() {
         JFrame frame = new JFrame();
-        JButton button = new JButton("click me like you mean it");
-        frame.getContentPane().add(BorderLayout.EAST, button);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JButton east = new JButton("EAST");
+        JButton west = new JButton("WEST");
+        JButton north = new JButton("NORTH");
+        JButton south = new JButton("SOUTH");
+        JButton center = new JButton("CENTER");
+
+        frame.getContentPane().add(BorderLayout.EAST, east);
+        frame.getContentPane().add(BorderLayout.WEST, west);
+        frame.getContentPane().add(BorderLayout.NORTH, north);
+        frame.getContentPane().add(BorderLayout.SOUTH, south);
+        frame.getContentPane().add(BorderLayout.CENTER, center);
         frame.setSize(200, 200);
         frame.setVisible(true);
     }
