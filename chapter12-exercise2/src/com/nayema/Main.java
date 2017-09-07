d93cc4653f64006c29c4a34989fdc01f3db61caa
Nayema Nasir <nayema.nasir89@gmail.com>
2017-09-07 14:19:21 -0400

    Makes a circle moving across the screen animation

---
+++ chapter12-exercise2/src/com/nayema/Main.java
@@ -0,0 +1,46 @@
+package com.nayema;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+    int x = 70;
+    int y = 70;
+
+    public static void main(String[] args) {
+        Main gui = new Main();
+        gui.go();
+    }
+
+    private void go() {
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        MyDrawPanel drawPanel = new MyDrawPanel();
+
+        frame.getContentPane().add(drawPanel);
+        frame.setSize(300, 300);
+        frame.setVisible(true);
+
+        for (int i = 0; i < 130; i++) {
+            x++;
+            y++;
+            drawPanel.repaint();
+
+            try {
+                Thread.sleep(50);
+            } catch (Exception ex) {
+            }
+        }
+    }
+
+    class MyDrawPanel extends JPanel {
+        public void paintComponent(Graphics g) {
+            g.setColor(Color.white);
+            g.fillRect(0, 0, this.getWidth(), this.getHeight());
+
+            g.setColor(Color.orange);
+            g.fillOval(x,y,40,40);
+        }
+    }
+}
