d441ec8ad5ad2d29b4568b217d7212afcd38957d
Nayema Nasir <nayema.nasir89@gmail.com>
2017-09-07 13:00:17 -0400

    Adds two inner classes for two separate GUI buttons

--- chapter12-exercise1/src/com/nayema/Main.java
+++ chapter12-exercise1/src/com/nayema/Main.java
@@ -5,33 +5,47 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class Main implements ActionListener {
-    JButton button;
-    JFrame frame;
-    MyDrawPanel drawPanel;
+public class Main {
+    public JFrame frame;
+    public JLabel label;
 
     public static void main(String[] args) {
         Main gui = new Main();
         gui.go();
     }
 
-    public void go() {
+    private void go() {
         frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        button = new JButton("Change Colors");
-        button.addActionListener(this);
+        JButton labelButton = new JButton("Change Label");
+        labelButton.addActionListener(new LabelListener());
 
-        drawPanel = new MyDrawPanel();
+        JButton colorButton = new JButton("Change Circle");
+        colorButton.addActionListener(new ColorListener());
 
-        frame.getContentPane().add(BorderLayout.SOUTH, button);
+        label = new JLabel("I'm a label");
+        MyDrawPanel drawPanel = new MyDrawPanel();
+
+        frame.getContentPane().add(BorderLayout.SOUTH, colorButton);
         frame.getContentPane().add(BorderLayout.CENTER, drawPanel);
+        frame.getContentPane().add(BorderLayout.EAST, labelButton);
+        frame.getContentPane().add(BorderLayout.WEST, label);
         frame.setSize(300, 300);
         frame.setVisible(true);
     }
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        frame.repaint();
+    class LabelListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            label.setText("Ouch!");
+        }
+    }
+
+    class ColorListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            frame.repaint();
+        }
     }
 }
