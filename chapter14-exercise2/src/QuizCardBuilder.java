e31796b1106dc70eb0f004b156ac5c4261151d54
Nayema Nasir <nayema.nasir89@gmail.com>
2017-09-15 18:34:40 -0400

    Creates a QuizCardBuilder which creates a QuizCard

---
+++ chapter14-exercise2/src/QuizCardBuilder.java
@@ -0,0 +1,93 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class QuizCardBuilder {
+
+    private JFrame frame;
+    private JPanel mainPanel;
+    private JTextArea question;
+    private JTextArea answer;
+    private JButton nextButton;
+    private JLabel questionLabel;
+    private JLabel answerLabel;
+    private ArrayList<QuizCard> cardList;
+
+    public QuizCardBuilder() {
+        nextButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                QuizCard card = new QuizCard(question.getText(), answer.getText());
+                cardList.add(card);
+                clearCard();
+            }
+        });
+    }
+
+    public void buildQuizCard() {
+        cardList = new ArrayList<>();
+
+        JMenuBar menuBar = new JMenuBar();
+        
+        JMenu fileMenu = new JMenu("File");
+        JMenuItem newMenuItem = new JMenuItem("New");
+        newMenuItem.addActionListener(new NewMenuListener());
+        fileMenu.add(newMenuItem);
+
+        JMenuItem saveMenuItem = new JMenuItem("Save");
+        saveMenuItem.addActionListener(new SaveMenuListener());
+        fileMenu.add(saveMenuItem);
+
+        frame = new JFrame("Quiz Card Builder");
+        frame.setContentPane(mainPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+
+    private void clearCard() {
+        question.setText("");
+        answer.setText("");
+        question.requestFocus();
+    }
+
+    private void saveFile(File selectedFile) {
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(selectedFile));
+            for (QuizCard card : cardList) {
+                writer.write(card.getQuestion() + "/");
+                writer.write(card.getAnswer() + "\n");
+            }
+            writer.close();
+        } catch (IOException ex) {
+            System.out.println("couldn't write the cardlist out.");
+            ex.printStackTrace();
+        }
+    }
+
+    private class NewMenuListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            cardList.clear();
+            clearCard();
+        }
+    }
+
+    private class SaveMenuListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            QuizCard card = new QuizCard(question.getText(), answer.getText());
+            cardList.add(card);
+
+            JFileChooser fileSave = new JFileChooser();
+            fileSave.showSaveDialog(frame);
+            saveFile(fileSave.getSelectedFile());
+        }
+    }
+}
