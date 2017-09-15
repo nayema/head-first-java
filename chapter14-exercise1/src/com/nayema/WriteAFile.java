263761a455f984ad623a6be4bed8e2ce9a5fc1a3
Nayema Nasir <nayema.nasir89@gmail.com>
2017-09-15 17:35:30 -0400

    Writer a new file to the project folder

---
+++ chapter14-exercise1/src/com/nayema/WriteAFile.java
@@ -0,0 +1,17 @@
+package com.nayema;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class WriteAFile {
+
+    public static void main(String[] args) {
+        try {
+            FileWriter writer = new FileWriter("Foo.txt");
+            writer.write("hello foo!");
+            writer.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
